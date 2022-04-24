8594866f1045c4e7136b93b54e0f5f03aeac52ea
Nick Hawn <nick.hawn@atomicobject.com>
2022-04-23 20:09:43 -0400

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,14 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-        Hi, I am{' '}
-          <span className="text-yellow-600">
+        <h1 className="text-5xl font-bold">
+          Hi, I am{' '}
+          <span className="text-blue-600">
           Nick Hawn{' '}
           </span>
           a Software Developer <br/> 
           Based in Grand Rapids Michigan. 
         </h1>
-
-        <p className="mt-8 text-2xl">
-        <a
-          className="underline text-yellow-600 hover:text-yellow-700 mr-3"
-          href="mailto:nick@nickhawn.com">
-          Email
-        </a>
-        {' '}
-        <a
-          className="underline text-yellow-600 hover:text-yellow-700 mr-3"
-          href="https://www.linkedin.com/in/nickhawn/">
-          LinkedIn
-        </a>
-        </p>
       </main>
     </div>
   )
