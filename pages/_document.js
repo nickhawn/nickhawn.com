25b56b597176622ca9ebab28ff9752d4eadd17b8
Nick Hawn <nick.hawn@atomicobject.com>
2023-01-01 20:30:06 -0500

    feat: redesign w/ Arc theme

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head />
-        <body>
+        <body className='background'>
           <Main />
           <NextScript />
         </body>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,39 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 background">
       <Head>
         <meta charSet="utf-8" />
         <title>Nick Hawn | Software Developer Based In Grand Rapids</title>
-        <meta name="description" content="Hi, I am Nick Hawn a Software Developer based in Grand Rapids Michigan."></meta>
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Hi, I am Nick Hawn a Software Developer based in Grand Rapids Michigan."
+        ></meta>
+        <link rel="icon" href="/logo.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-5xl font-bold">
-          Hi, I am{' '}
-          <span className="text-blue-600">
-          Nick Hawn,{' '}
-          </span>
-          a Software Developer <br/> 
-          Based in Grand Rapids, Michigan. 
+        <h1 className="text-3xl font-bold">
+          Hi, I am <span className="primary-color">Nick Hawn</span>, a Software
+          Developer <br />
+          Based in Grand Rapids, Michigan.
         </h1>
+        <p className="mt-3 text-md">
+          Software Developer & Consultant at{" "}
+          <a href="https://www.atomicobject.com/" className="underline">
+            Atomic Object
+          </a>
+        </p>
       </main>
+      <div className="flex flex-row justify-start justify-items-start justify-self-start">
+        <a
+          href="https://spin.atomicobject.com/author/nick-hawn/"
+          className="flex justify-start text-sm gradient"
+        >
+          Latest Blog Post
+        </a>
+      </div>
     </div>
-  )
+  );
 }
