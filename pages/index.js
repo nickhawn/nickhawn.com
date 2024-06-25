42e6434f7a4e83b4c86802a967e1f0d93a4b9bf4
Nicholas Hawn <nickhawn@hey.com>
2024-06-25 16:29:36 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 background">
+    <div className="flex flex-col items-center justify-center h-screen background">
       <Head>
         <meta charSet="utf-8" />
         <title>Nick Hawn | Software Developer Based In Grand Rapids</title>
@@ -21,17 +21,24 @@ export default function Home() {
         </h1>
         <p className="mt-3 text-md">
           Software Developer & Consultant at{" "}
-            <a href="https://www.atomicobject.com/" className="underline">
+          <a href="https://www.atomicobject.com/" className="underline">
             Atomic Object
           </a>
         </p>
       </main>
-      <div className="flex flex-row justify-start justify-items-start justify-self-start">
+      <div className="flex flex-row items-center justify-center mt-4">
         <a
           href="https://spin.atomicobject.com/author/nick-hawn/"
-          className="flex justify-start text-sm gradient"
+          className="flex justify-center text-sm gradient"
         >
           Latest Blog Post
+        </a>
+        <div className="h-4 w-0.5 mx-2 background-primary-color"></div>
+        <a
+          href="https://www.linkedin.com/in/nickhawn/"
+          className="flex justify-center text-sm gradient"
+        >
+          Linkedin
         </a>
       </div>
     </div>
